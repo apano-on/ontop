6b9106bf32c98c31ea4449cccb54e5a5852a88d6
apano-on <albulen.pano@unibz.it>
2023-05-31 22:48:18 +0200

    Add recursion for ST_ASTEXT

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/geof/GeofIntersectionFunctionSymbolImpl.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/geof/GeofIntersectionFunctionSymbolImpl.java
@@ -3,6 +3,7 @@ package it.unibz.inf.ontop.model.term.functionsymbol.impl.geof;
 import com.google.common.collect.ImmutableList;
 import it.unibz.inf.ontop.model.term.ImmutableTerm;
 import it.unibz.inf.ontop.model.term.TermFactory;
+import it.unibz.inf.ontop.model.type.DBTypeFactory;
 import it.unibz.inf.ontop.model.type.RDFDatatype;
 import org.apache.commons.rdf.api.IRI;
 
@@ -12,6 +13,19 @@ import java.util.function.BiFunction;
 public class GeofIntersectionFunctionSymbolImpl extends AbstractBinaryGeofWKTFunctionSymbolDirectImpl {
     public GeofIntersectionFunctionSymbolImpl(@Nonnull IRI functionIRI, RDFDatatype wktLiteralType) {
         super("GEOF_INTERSECTION", functionIRI, ImmutableList.of(wktLiteralType, wktLiteralType), wktLiteralType);
+    }
+
+    @Override
+    protected ImmutableTerm computeDBTerm(ImmutableList<ImmutableTerm> subLexicalTerms, ImmutableList<ImmutableTerm> typeTerms, TermFactory termFactory) {
+
+        WKTLiteralValue v0 = GeoUtils.extractWKTLiteralValue(termFactory, subLexicalTerms.get(0));
+        WKTLiteralValue v1 = GeoUtils.extractWKTLiteralValue(termFactory, subLexicalTerms.get(1));
+
+        if (!v0.getSRID().equals(v1.getSRID())) {
+            throw new IllegalArgumentException(String.format("SRIDs do not match: %s and %s", v0.getSRID(), v1.getSRID()));
+        }
+
+        return termFactory.getDBAsText(getDBFunction(termFactory).apply(v0.getGeometry(), v1.getGeometry()).simplify());
     }
 
     @Override
