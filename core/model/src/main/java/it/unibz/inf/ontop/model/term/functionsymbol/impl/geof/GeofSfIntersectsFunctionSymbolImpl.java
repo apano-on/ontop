4b547d8edc35078013721a04b4cfd5d7959c34c0
apano-on <albulen.pano@unibz.it>
2023-05-31 22:13:32 +0200

    Add recursion for ST_ASTEXT

--- core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/geof/GeofSfIntersectsFunctionSymbolImpl.java
+++ core/model/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/impl/geof/GeofSfIntersectsFunctionSymbolImpl.java
@@ -1,11 +1,11 @@
 package it.unibz.inf.ontop.model.term.functionsymbol.impl.geof;
 
 import com.google.common.collect.ImmutableList;
-import it.unibz.inf.ontop.model.term.ImmutableFunctionalTerm;
 import it.unibz.inf.ontop.model.term.ImmutableTerm;
 import it.unibz.inf.ontop.model.term.TermFactory;
 import it.unibz.inf.ontop.model.type.DBTypeFactory;
 import it.unibz.inf.ontop.model.type.RDFDatatype;
+import it.unibz.inf.ontop.model.type.TermTypeInference;
 import org.apache.commons.rdf.api.IRI;
 
 import javax.annotation.Nonnull;
@@ -35,19 +35,16 @@ public class GeofSfIntersectsFunctionSymbolImpl extends AbstractGeofBooleanFunct
          * defined there is an error since PostGIS may not know which function to choose.
          * @see https://postgis.net/docs/ST_Intersects.html
          */
-        //TODO: If buffer in string, do not cast to geography!
-        //TODO: What about TEXT?
+
         if (dbTypeFactory.supportsDBGeographyType()) {
-            ImmutableTerm term0 = (!v0.getGeometry().isGround() && v0.getGeometry().toString().contains("ST_BUFFER"))
+            boolean bufferCondition = (!v0.getGeometry().isGround() && v0.getGeometry().toString().contains("ST_BUFFER"));
+            ImmutableTerm term0 = bufferCondition
                     ? v0.getGeometry()
                     : termFactory.getDBCastFunctionalTerm(dbTypeFactory.getDBGeographyType(), v0.getGeometry());
-            ImmutableTerm term1 = (!v1.getGeometry().isGround() && v1.getGeometry().toString().contains("ST_BUFFER"))
+            ImmutableTerm term1 = bufferCondition
                     ? v1.getGeometry()
                     : termFactory.getDBCastFunctionalTerm(dbTypeFactory.getDBGeographyType(), v1.getGeometry());
-            return getDBFunction(termFactory).apply(
-                    unwrapSTAsText(term0),
-                    unwrapSTAsText(term1))
-                    .simplify();
+            return getDBFunction(termFactory).apply(term0, term1).simplify();
 
         }
 
@@ -57,17 +54,5 @@ public class GeofSfIntersectsFunctionSymbolImpl extends AbstractGeofBooleanFunct
     @Override
     public BiFunction<ImmutableTerm, ImmutableTerm, ImmutableTerm> getDBFunction(TermFactory termFactory) {
         return termFactory::getDBSTIntersects;
-    }
-
-    private ImmutableTerm unwrapSTAsText(ImmutableTerm term) {
-        return Optional.of(term)
-                // term is a function
-                .filter(t -> t instanceof ImmutableFunctionalTerm).map(ImmutableFunctionalTerm.class::cast)
-                // the function symbol is ST_ASTEXT
-                .filter(t -> t.getFunctionSymbol().getName().startsWith("ST_ASTEXT"))
-                // extract the 0-th argument
-                .map(t -> t.getTerm(0))
-                // otherwise the term itself
-                .orElse(term);
     }
 }
