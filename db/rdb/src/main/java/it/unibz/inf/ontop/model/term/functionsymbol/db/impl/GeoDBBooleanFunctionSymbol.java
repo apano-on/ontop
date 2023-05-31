e2ea0a46e191c6a5cf9edb2fe14a8d8a988c123e
apano-on <albulen.pano@unibz.it>
2023-05-31 22:12:43 +0200

    Add recursion for ST_ASTEXT

--- db/rdb/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/GeoDBBooleanFunctionSymbol.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/model/term/functionsymbol/db/impl/GeoDBBooleanFunctionSymbol.java
@@ -5,12 +5,9 @@ import it.unibz.inf.ontop.iq.node.VariableNullability;
 import it.unibz.inf.ontop.model.term.ImmutableFunctionalTerm;
 import it.unibz.inf.ontop.model.term.ImmutableTerm;
 import it.unibz.inf.ontop.model.term.TermFactory;
+import it.unibz.inf.ontop.model.term.functionsymbol.FunctionSymbol;
 import it.unibz.inf.ontop.model.type.DBTermType;
-import it.unibz.inf.ontop.model.type.DBTypeFactory;
 import it.unibz.inf.ontop.utils.ImmutableCollectors;
-
-import java.util.Optional;
-import java.util.stream.Stream;
 
 
 public class GeoDBBooleanFunctionSymbol extends DefaultSQLSimpleDBBooleanFunctionSymbol {
@@ -32,32 +29,14 @@ public class GeoDBBooleanFunctionSymbol extends DefaultSQLSimpleDBBooleanFunctio
 
     // if term is ST_ASTEXT(arg), returns arg, otherwise the term itself
     private ImmutableTerm unwrapSTAsText(ImmutableTerm term, TermFactory termFactory) {
-
-        ImmutableTerm temp0 = Optional.of(term)
-                // term is a function
-                .filter(t -> t instanceof ImmutableFunctionalTerm).map(ImmutableFunctionalTerm.class::cast)
-                // the function symbol is ST_ASTEXT
-                .filter(t -> t.getFunctionSymbol().getName().startsWith("ST_ASTEXT"))
-                // extract the 0-th argument
-                .map(t -> t.getTerm(0))
-                // otherwise the term itself
-                .orElse(term);
-        if (temp0.toString().contains("ST_ASTEXT")) {
-            Optional<ImmutableTerm> temp1 = Optional.of(temp0)
-                    // term is a function
-                    .filter(t -> t instanceof ImmutableFunctionalTerm).map(ImmutableFunctionalTerm.class::cast)
-                    // there is an ST_ASTEXT defined somewhere
-                    .filter(t -> t.toString().startsWith("toGEOGRAPHY") && t.getTerm(0).toString().startsWith("ST_ASTEXT"))
-                    // extract the 0-th argument
-                    .map(t -> t.getTerm(0))
-                    .stream()
-                    .map(t2 -> ((ImmutableFunctionalTerm) t2).getTerm(0)).findFirst();
-            DBTypeFactory dbTypeFactory = termFactory.getTypeFactory().getDBTypeFactory();
-            return (temp1.isEmpty())
-                    ? temp0
-                    : termFactory.getDBCastFunctionalTerm(dbTypeFactory.getDBGeographyType(), temp1.get());
-        } else {
-            return temp0;
+        if (term instanceof ImmutableFunctionalTerm) {
+            ImmutableFunctionalTerm functionalTerm = (ImmutableFunctionalTerm) term;
+            FunctionSymbol functionSymbol = functionalTerm.getFunctionSymbol();
+            if (functionSymbol.getName().startsWith("ST_ASTEXT")) {
+                ImmutableTerm argument = functionalTerm.getTerm(0);
+                return unwrapSTAsText(argument, termFactory);
+            }
         }
+        return term;
     }
 }
